eab1fbd733fed6199d373d49521d7525ce78af3e
lee unseo <lee.unseo17@gmail.com>
2022-12-11 17:49:18 +0900

    add style file

--- src/components/header/navbar.rs
+++ src/components/header/navbar.rs
@@ -4,7 +4,7 @@ use yew::{function_component, html, Html};
 #[function_component]
 pub fn Navbar() -> Html {
     html! {
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar fixed-top navbar-dark bg-dark">
             <div class="container-fluid d-flex flex-row">
                 <a class="navbar-brand h1 mx-auto">{ "awawa" }</a>
                 <NavMenuButton />
